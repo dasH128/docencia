2d2b402580d6c787c65825baa00f2d34350bd216
dash <rojas71421@gmail.com>
2017-05-29 15:49:27 -0500

    proyecto base

---
+++ app/src/main/java/com/climper/docenciaapp/ui/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.climper.docenciaapp.ui.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.climper.docenciaapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
